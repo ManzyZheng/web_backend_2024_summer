c08630973f1fb9cccce28c321cc0ab589259b4df
Manzy <3236583972@qq.com>
2024-07-27 13:07:54 +0800

    initial

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1721717716089_502',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
